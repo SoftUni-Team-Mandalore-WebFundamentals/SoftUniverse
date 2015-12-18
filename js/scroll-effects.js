b3a4b12e4e8316cd9c836075d9e1be4b307cf222
Inspix <inspix89@gmail.com>
2015-12-19 01:23:16 +0200

    Values finetune

--- js/scroll-effects.js
+++ js/scroll-effects.js
@@ -3,17 +3,16 @@ var tags = [];
 $(function(){
   window.addEventListener("scroll", onScroll);
 
-
-  tags.push({at: 970, active:false, obj: function(){
+  tags.push({at: 1000, active:false, obj: function(){
     return $('main h2');
   }});
-  tags.push({at: 1100, active:false, obj: function(){
-    return $('.story-text:not(".shown")').first();
-  }});
-  tags.push({at: 1500, active:false, obj: function(){
+  tags.push({at: 1300, active:false, obj: function(){
     return $('.story-text:not(".shown")').first();
   }});
   tags.push({at: 1800, active:false, obj: function(){
+    return $('.story-text:not(".shown")').first();
+  }});
+  tags.push({at: 2100, active:false, obj: function(){
     return $('.story-text:not(".shown")').first();
   }});
   onScroll();
@@ -32,26 +31,4 @@ function onScroll(){
       break;
     }
   }
-
-
-
-  // if (viewport > 970 && !tags["header"]) {
-  //   $('main h2').addClass('shown');
-  //   tags["header"] = true
-  // }
-  // if (viewport > 1100 && !tags["p1"]) {
-  //   $('.story-text:not(".shown")').first().addClass('shown');
-  //   tags["p1"] = true
-  // }
-  //
-  // if (viewport > 1500 && !tags["p2"]) {
-  //   $('.story-text:not(".shown")').first().addClass('shown');
-  //   tags["p2"] = true
-  // }
-  //
-  // if (viewport > 1800 && !tags["p3"]) {
-  //   $('.story-text:not(".shown")').first().addClass('shown');
-  //   tags["p3"] = true
-  // }
-
 }
