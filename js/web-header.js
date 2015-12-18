dcc5a0566b82aca2d6902d16bcbb9ceb2f637939
nedyalkovV <nedyalkov_v@yahoo.com>
2015-12-18 22:14:13 +0200

    responsive menu

--- js/web-header.js
+++ js/web-header.js
@@ -1,21 +1,62 @@
 var header = document.getElementById('header');
 var logo = document.getElementById('logo');
 var headermain = document.getElementById('header-main');
+var navi = document.getElementById('menu');
 var yPos;
+var xWide;
+var isExpand = false;
+
+jQuery(function () {
+    $('#nav-btn').click(function () {
+        $( "#menu" ).slideToggle("fast");
+        var w = window.innerWidth;
+        var header = document.getElementById('header');
+        header.style.background = "#353d40";
+        if (isExpand == false) {
+          isExpand = true;
+        }else{
+          isExpand = false;
+        }
+    });
+});
+
 function runOnScroll(){
   yPos = window.pageYOffset;
+  var w = window.innerWidth;
   if (yPos>80) {
     header.style.paddingTop = "5px";
     header.style.paddingBottom = "0px";
     logo.style.width="320px";
     headermain.style.borderBottom = "0px solid silver";
     header.style.background = "#353d40";
+    navi.style.marginTop = "20px";
+    if (isExpand == true && w<=1080) {
+      $( "#menu" ).slideToggle("fast");
+      isExpand = false;
+    }
   }else {
     header.style.paddingTop = "20px";
     header.style.paddingBottom = "20px";
     header.style.background = "none";
     logo.style.width="420px";
     headermain.style.borderBottom = "1px solid silver";
+    navi.style.marginTop = "30px";
+    if (isExpand == true ) {
+      $( "#menu" ).slideToggle("fast");
+      isExpand = false;
+    }
   }
+  console.log(isExpand);
 }
+$(window).resize(function() {
+  var w = window.innerWidth;
+  if (w>=1080) {
+      document.getElementById("menu").style.display = "block";
+      isExpand = false;
+  }
+  if (w<=1080) {
+    document.getElementById("menu").style.display = "none";
+    isExpand = false;
+  }
+});
 window.addEventListener("scroll", runOnScroll);
