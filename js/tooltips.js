88bc2ff2099724f617812144bc7139e44be369f7
nedyalkovV <nedyalkov_v@yahoo.com>
2015-12-20 01:59:54 +0200

    tooltips added

---
+++ js/tooltips.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#leia').tooltipster({
+      content: $('<span><b>Nickname:</b> kitsune0408</span><br><span><b>Име:</b> Alexandra Rybakova</span><br><span><b>Статус:</b> Принцеса</span><br><span style="color:silver"><b>Девиз:</b> Ако пободим империята, ще има <br> кебабчета за всеки</span>')
+  });
+  $('#yoda').tooltipster({
+      content: $('<span><b>Nickname:</b> nedyalkov_v</span><br><span><b>Име:</b> Vasil Nedyalkov</span><br><span><b>Статус:</b> Учител джадай</span><br><span style="color:silver"><b>Девиз:</b> Страхът води до гняв, гневът води до <br> омраза, омразата води до десет ракий и една <br> кисела краставичка.</span>')
+  });
+  $('#chubaka').tooltipster({
+      content: $('<span><b>Nickname:</b> Inspix</span><br><span><b>Име:</b> Yuliyan Rusev</span><br><span><b>Статус:</b> Бунтовник</span><br><span style="color:silver"><b>Девиз:</b> Най-добър приятел на Хан Соло, завинаги...</span>')
+  });
+  $('#moul').tooltipster({
+      content: $('<span><b>Nickname:</b> rulezzzbg</span><br><span><b>Име:</b> Denislav Temeliev</span><br><span><b>Статус:</b> Ситски Лорд</span><br><span style="color:silver"><b>Девиз:</b> Що съм червен и аз не знам?!...</span>')
+  });
+  $('#luke').tooltipster({
+      content: $('<span><b>Nickname:</b> zdravko7</span><br><span><b>Име:</b> Zdravko Botushanov</span><br><span><b>Статус:</b> Уж съм джадай</span><br><span style="color:silver"><b>Девиз:</b> Оби ми каза, че силата ще ми <br>помага на изпита в SoftUni</span>')
+  });
+  $('#obi').tooltipster({
+      content: $('<span><b>Nickname:</b> bojidar.tonchev</span><br><span><b>Име:</b> Bozhidar Tonchev</span><br><span><b>Статус:</b> Учител джадай</span><br><span style="color:silver"><b>Девиз:</b> Нека силата бъде с мен, а вие <Br> си гледайте работата...</span>')
+  });
+});
