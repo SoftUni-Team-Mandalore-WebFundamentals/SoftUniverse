bb21f1b73cf1577758179f938465c31e0e01bd6c
Inspix <inspix89@gmail.com>
2015-12-22 20:50:02 +0200

    Fixed some issues with the cursor, position on refreshing, auto focus on loading and focus on click on the whole cmd window

--- js/console.js
+++ js/console.js
@@ -1,27 +1,45 @@
 $(function () {
-    var cursor;
-    $('#cmd').click(function () {
-        $('input').focus();
-        cursor = window.setInterval(function () {
-            if ($('#cursor').css('visibility') === 'visible') {
-                $('#cursor').css({
-                    visibility: 'hidden'
-                });
-            } else {
-                $('#cursor').css({
-                    visibility: 'visible'
-                });
-            }
-        }, 500);
+    $.fn.setCursorAtEnd = function(){
+      var content = this.val();
+      console.log(content);
+      this.val('').val(content);
+    };
 
+    var cursor;
+    var input = $('input');
+    var cmdSpan = $('#cmd span');
+    cmdSpan.text(input.val());
+
+    input.focus(function(){
+      cursor = window.setInterval(function () {
+          if ($('#cursor').css('visibility') === 'visible') {
+              $('#cursor').css({
+                  visibility: 'hidden'
+              });
+          } else {
+              $('#cursor').css({
+                  visibility: 'visible'
+              });
+          }
+      }, 500);
     });
-    $('input').keyup(function () {
-        $('#cmd span').text($(this).val());
+
+    input.setCursorAtEnd();
+    input.focus();
+
+    input.keyup(function () {
+        cmdSpan.text($(this).val());
     });
-    $('input').blur(function () {
+
+    input.blur(function () {
         clearInterval(cursor);
         $('#cursor').css({
             visibility: 'visible'
         });
+    });
+
+    $('.r2-console').click(function(){
+      input.setCursorAtEnd();
+      input.focus();
     });
 });
